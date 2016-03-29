a061b430d6d6d572d3bc25d430e1e25af7a4c0ed
srikar-bhargav-durgi <sdurgi17@gmail.com>
2016-03-29 19:14:58 +0530

    moved style.css to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 module.exports = {
     entry: "./src/js/app.js",
     output: {
-        path: __dirname,
-        filename: "dist/bundle.js"
+        path: __dirname + "/dist",
+        filename: "bundle.js"
     },
     module: {
         loaders: [
             { test: /\.css$/, loader: "style!css" }
         ]
+    },
+    devServer: {
+        contentBase: './dist',
+        host: "0.0.0.0",
+        port: "8000"
     }
 };
